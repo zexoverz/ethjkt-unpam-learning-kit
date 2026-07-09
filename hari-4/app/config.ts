76cb8ecc5bf57335872b14442268ac062b89fef9
dikarahmat <dikarahmat998@gmail.com>
2026-07-10 01:01:01 +0700

    Finish Day 4: DEX deployed, swap executed, interface functional

--- hari-4/app/config.ts
+++ hari-4/app/config.ts
@@ -1,33 +1,28 @@
 // ============================================================
-// KONFIG KAMPUSSWAP  —  >>> MURID CUKUP EDIT FILE INI SAJA <<<
-//
-// Ganti alamat contract + logo token jadi punyamu sendiri.
-// Logo: taruh file gambar di folder "public/", lalu tulis path-nya
-// "/namafile.png" (awali garis miring) di "logo".
+// KONFIG KAMPUSSWAP 
 // ============================================================
 
 export const CONFIG = {
   // Sepolia testnet.
   SEPOLIA_CHAIN_ID: 11155111,
 
-  // RPC publik Sepolia -> dipakai buat BACA data pool tanpa perlu wallet.
+  // RPC publik Sepolia
   RPC_URL: "https://ethereum-sepolia-rpc.publicnode.com",
 
-  // WalletConnect projectId (buat RainbowKit). GRATIS: bikin di
-  // https://cloud.reown.com -> New Project -> copy Project ID.
-  // Kalau kosong, connect MetaMask masih jalan, tapi QR WalletConnect nggak.
+  // Bisa kamu isi kalau mau pakai WalletConnect, kalau cuma MetaMask 
+  // boleh dikosongkan atau biarkan seperti itu.
   WALLETCONNECT_PROJECT_ID: "GANTI_DENGAN_PROJECT_ID_KAMU",
 
-  // Alamat pool AMM kamu (hasil deploy SimpleAMM di Remix).
-  AMM_ADDRESS: "0x60a19Da3F8CFA6F64a35a374CE0e5a7bC2d695c3",
+  // GANTI DENGAN ALAMAT SIMPLEAMM KAMU DIBAWAH INI:
+  AMM_ADDRESS: "0x519A58F552d4aF1400cF40E3505c55BFC3Dcb78e",
 
-  // TOKEN A = KOIN KAMU (harus SAMA dengan tokenA di SimpleAMM).
-  TOKEN_A: {
-    address: "0x8cfd81e42052a502da01a0884F4De804d0C1Eb4B",
-    logo: "/zexoverz.webp", // ganti dengan logo koinmu (file di public/)
+TOKEN_A: {
+    address: "0x52BB960d6c0e57ff09897E0a02A560B3e81b7509",
+    logo: "/dkt-logo.png",
+    symbol: "DKT", // Tambahkan baris ini
   },
 
-  // TOKEN B = ETHJKT (token bersama dari pengajar).
+  // TOKEN B = ETHJKT (Token dari pengajar)
   TOKEN_B: {
     address: "0x7E96fed902B0A26b62DA78e8112253920Fc55936",
     logo: "/ethjkt-logo.png",
