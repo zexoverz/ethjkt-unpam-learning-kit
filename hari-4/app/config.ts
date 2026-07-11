082e025d4785bf70c2db67991e3d243c0b97f876
sindi213 <sndyc762@gmail.com>
2026-07-11 10:44:58 +0700

    update hari-4

--- hari-4/app/config.ts
+++ hari-4/app/config.ts
@@ -19,12 +19,12 @@ export const CONFIG = {
   WALLETCONNECT_PROJECT_ID: "GANTI_DENGAN_PROJECT_ID_KAMU",
 
   // Alamat pool AMM kamu (hasil deploy SimpleAMM di Remix).
-  AMM_ADDRESS: "0x60a19Da3F8CFA6F64a35a374CE0e5a7bC2d695c3",
+  AMM_ADDRESS: "0x5307925df061398795c75850d74F5a9E1D31E015",
 
   // TOKEN A = KOIN KAMU (harus SAMA dengan tokenA di SimpleAMM).
   TOKEN_A: {
-    address: "0x8cfd81e42052a502da01a0884F4De804d0C1Eb4B",
-    logo: "/zexoverz.webp", // ganti dengan logo koinmu (file di public/)
+    address: "0x54081934A1CF2643a272118502fd333a5DC2019B",
+    logo: "/bubu.jpg", // ganti dengan logo koinmu (file di public/)
   },
 
   // TOKEN B = ETHJKT (token bersama dari pengajar).
